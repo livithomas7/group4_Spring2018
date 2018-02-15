da7f91f2fec6042cfb695e5832dc27941d4a8c5a
livithomas7 <livithomas7@gmail.com>
2018-02-15 23:58:11 +0000

    added a tenant page, draft

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,36 +1,17 @@
 class MoviesController < ApplicationController
 
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:tenants).permit(:dashboard, :maintenance, :message, :documents, :paymen)
   end
 
   def show
     id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
+    @movies = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
   def index
-    @all_ratings = Movie.ratings.map { |rating| {name: rating, value: params[:ratings] ? params[:ratings].include?(rating) : true} }
-    @movies = params[:ratings]&.any? ? Movie.find_all_by_ratings(params[:ratings].keys) : Movie.all
-
-    if params[:sort]
-      @movies = @movies.order(params[:sort])
-    end
-
-    session[:sort] = params[:sort] if params[:sort]
-    session[:ratings] = params[:ratings] if params[:ratings] || params[:commit] == 'Refresh'
-
-    if (!params[:sort] && !params[:ratings]) && (session[:sort] && session[:ratings])
-      flash.keep
-      return redirect_to movies_path(sort: session[:sort], ratings: session[:ratings])
-    elsif !params[:sort] && session[:sort]
-      flash.keep
-      return redirect_to movies_path(sort: session[:sort], ratings: params[:ratings])
-    elsif !params[:ratings] && session[:ratings]
-      flash.keep
-      return redirect_to movies_path(sort: params[:sort], ratings: session[:ratings])
-    end
+    @movies = Movie.all
   end
 
   def new
--- db/migrate/20150809022253_create_movies.rb
+++ db/migrate/20150809022253_create_movies.rb
@@ -1,10 +1,11 @@
 class CreateMovies < ActiveRecord::Migration
   def change
-    create_table :movies do |t|
-      t.string :title
-      t.string :rating
-      t.text :description
-      t.datetime :release_date
+    create_table :tenant do |t|
+      t.string :dashboard
+      t.string :maintenance
+      t.string :message
+      t.string :documents
+      t.string :payment
       # Add fields that let Rails automatically keep track
       # of when movies are added or modified:
       t.timestamps
